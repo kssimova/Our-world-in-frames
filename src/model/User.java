6e90d2c3800a509ee087a0028fd27dd4522b9936
MariyaYo <black__sea@abv.bg>
2017-04-06 17:26:10 +0300

    pojo validations

--- src/model/User.java
+++ src/model/User.java
@@ -7,9 +7,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class User {
+import javax.xml.bind.ValidationException;
+
+public class User implements Comparable<User>{
 	enum Gender {MALE, FEMALE}
+	private final String EMAIL_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9]+.[a-z.]+$";
+	private final String MOBILEPHONE_PATTERN = "([08]{2}+[0-9]{8})";
 	
 	private String username;
 	private String email;
@@ -19,152 +25,226 @@ public class User {
 	private String country;
 	private String city;
 	private String descriprion;
+	private String mobileNumber;
 	private Date birthdate;
-	private Gender gender = Gender.FEMALE;
+	private Gender gender;
 	private String profilePhotoPath;
 	private TreeMap <String, Album> albums;
 	private TreeSet <User> followers;
 	private TreeSet <User> following;
+	private Pattern pattern = null;
+	private Matcher matcher = null;
 	
 	//again builder pattern
-	public User(String username, String email, String password) {
-		this.username = username;
-		this.email = email;
-		this.password = password;
+	public User(String username, String email, String password) throws ValidationException {
+		if(validUsername(username)){
+			this.username = username;
+		}else{
+			throw new ValidationException("Username not valid");
+		}
+		if(validEmail(email)){
+			this.email = email;
+		}else{
+			throw new ValidationException("User email not valid");
+		}
+		if(validPassword(password)){
+			this.password = password;
+		}else{
+			throw new ValidationException("User password not valid");
+		}
+		this.albums = new TreeMap<>();
+		this.followers = new TreeSet<>();
+		this.following = new TreeSet<>();
 	}
 	
-	
-	public void changeEmail(String email) {
-		
-	}
-	
-	
-	public void changePassword(String password) {
-		
-	}
-	
-	
-	public void changeFirstName(String FirstName) {
-		
-	}
-	
-	
-	public void changeLastName(String lastNmae) {
-		
-	}
-	
-	
-	public void changeCountry(String country) {
-		
-	}
-	
-	
-	public void changeCity(String city) {
-		
-	}
-	
-	
-	public void changeDescription(String desc) {
-		
-	}
-	
-	
-	public void changeBirthDate(Date birthDate) {
-		
-	}
-	
-	
-	public void changeGender(Gender gender) {
-		
-	}
-	
-	
-	public void changeProfilePhoto(String photopath) {
-		
-	}
-	
-	
-	public void createAlbum(String albumName, String desc) {
-		this.albums.put(albumName, new Album(albumName, desc, LocalDateTime.now(), this));
-	}
-	
-	
-	public void addFollower(User user){
-		this.followers.add(user);
-	}
-	
-	
-	public void addFollowing(User user){
-		this.following.add(user);
-	}
-	
+	// getters
 	
 	public String getUsername() {
 		return username;
 	}
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
 	public String getFirstName() {
 		return firstName;
 	}
-
-
 	public String getLastName() {
 		return lastName;
 	}
-
-
 	public String getCountry() {
 		return country;
 	}
-
-
 	public String getCity() {
 		return city;
 	}
-
-
 	public String getDescriprion() {
 		return descriprion;
 	}
-
-
 	public Date getBirthdate() {
 		return birthdate;
 	}
-
-
 	public Gender getGender() {
 		return gender;
 	}
-
-
 	public String getProfilePhotoPath() {
 		return profilePhotoPath;
 	}
-
+		
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
 
 	public Map<String, Album> getAlbums()  {
 		return Collections.unmodifiableSortedMap(albums);
 	}
-
 	public Set<User> getFollowers() {
 		return Collections.unmodifiableSortedSet(followers);
 	}
-
 	public Set<User> getFollowing()  {
 		return Collections.unmodifiableSortedSet(following);
+	}	
+
+	// setters	
+	
+	public void changeEmail(String email) throws ValidationException {
+		if(validEmail(email)){
+			this.email = email;		
+		}else{
+			throw new ValidationException("User email not valid");
+		}
 	}
 	
+	public void changeMobileNumber(String mobileNumber) throws ValidationException{
+		if(patternFinder(MOBILEPHONE_PATTERN, mobileNumber)){
+			this.mobileNumber = mobileNumber;		
+		}else{
+			throw new ValidationException("User phone number not valid");
+		}
+	}
+	
+	public void changePassword(String password) throws ValidationException {
+		if (validPassword(password)) {
+			this.password = password;
+		}else{
+			throw new ValidationException("User password not valid");
+		}
+	}
+	
+	
+	public void changeFirstName(String firstName) throws ValidationException {
+		if (validateString(firstName) && firstName.length() <= 45) {
+			this.firstName = firstName;
+		}else{
+			throw new ValidationException("User first name not valid");
+		}
+	}
+	
+	public void changeLastName(String lastName) throws ValidationException {
+		if (validateString(lastName) && lastName.length() <= 45) {
+			this.lastName = lastName;
+		}else{
+			throw new ValidationException("User last name not valid");
+		}
+	}
+	
+	public void changeCountry(String country) throws ValidationException {
+		if (validateString(country) && country.length() <= 45) {
+			this.country = country;
+		}else{
+			throw new ValidationException("User country not valid");
+		}
+	}
+	
+	
+	public void changeCity(String city) throws ValidationException {
+		if (validateString(city) && city.length() <= 45) {
+			this.city = city;
+		}else{
+			throw new ValidationException("User city not valid");
+		}
+	}
+	
+	
+	public void changeDescription(String desc) throws ValidationException {
+		if (desc.length() <= 400) {
+			this.descriprion = desc;
+		}else{
+			throw new ValidationException("User description not valid");
+		}
+	}
+	
+	
+	public void changeBirthDate(Date birthDate) {
+		this.birthdate = birthDate;
+	}
+	
+	
+	public void changeGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+	
+	public void changeProfilePhoto(String picturePath) throws ValidationException {
+		if(picturePath.length() <= 500){
+			this.profilePhotoPath = picturePath;
+		}else{
+			throw new ValidationException("User profile picture not valid");
+		}
+	}
+		
+	public void createAlbum(String albumName, String desc,  long albumId) throws ValidationException {
+		this.albums.put(albumName, new Album(albumName, desc, LocalDateTime.now(), this, albumId));
+	}
+	
+	
+	public void addFollower(User user){
+		if(validUser(user)){
+			this.followers.add(user);
+		}
+	}
+	
+	
+	public void addFollowing(User user){
+		if(validUser(user)){
+			this.following.add(user);
+		}
+	}
+	
+	//validations
+	
+	public boolean patternFinder(String regex, String field) {
+		pattern = Pattern.compile(regex);
+		matcher = pattern.matcher(field);
+		return (matcher.matches() && !field.isEmpty() && field != null);
+	}
+	
+	public boolean validateString(String str){
+		return (!password.isEmpty() && password != null && password.length() > 3);
+	}
+	
+	private boolean validUser(User user){
+		return !user.equals(null);
+	}
+	
+	private boolean validUsername(String username) {
+		return (validateString(username) && username.length() <= 45);
+	}
+	
+	private boolean validEmail(String emal){
+		return (patternFinder(EMAIL_PATTERN, email));
+	}
+	private boolean validPassword(String password){
+		return (validateString(password) && password.length() <= 100);
+	}
+	
+	//compare to other Posts
+
+	@Override
+	public int compareTo(User user) {
+		return this.username.compareTo(user.username);
+	}
 	
 }
